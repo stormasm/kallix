b8f830f8626016ff0f5664b362969ad939feda38
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-04-04 21:19:27 -0700

    rename browse.rs tabxa_tabxb.rs

--- src/views.rs
+++ src/views.rs
@@ -1,15 +1,15 @@
 mod albums;
-mod browse;
 pub mod context_menu;
 mod dropdown;
 mod kallax;
 mod now_playing;
+mod tabxa_tabxb;
 pub mod tracks;
 
 pub use albums::Albums;
-pub use browse::Browse;
 pub use context_menu::ContextMenu;
 pub use dropdown::Dropdown;
 pub use kallax::Kallax;
 pub use now_playing::NowPlaying;
+pub use tabxa_tabxb::Browse;
 pub use tracks::Tracks;
