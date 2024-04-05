2b3c470a3c63b92b6556ab7d1e2680495e2731ef
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-04-04 21:22:37 -0700

    rename now_playing.rs tabxc_tabxd.rs

--- src/views.rs
+++ src/views.rs
@@ -2,14 +2,14 @@ mod albums;
 pub mod context_menu;
 mod dropdown;
 mod kallax;
-mod now_playing;
 mod tabxa_tabxb;
+mod tabxc_tabxd;
 pub mod tracks;
 
 pub use albums::Albums;
 pub use context_menu::ContextMenu;
 pub use dropdown::Dropdown;
 pub use kallax::Kallax;
-pub use now_playing::NowPlaying;
 pub use tabxa_tabxb::Browse;
+pub use tabxc_tabxd::NowPlaying;
 pub use tracks::Tracks;
