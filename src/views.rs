a74e5615572ac16a8797dcb268010c48b5964c7d
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-04-04 21:29:11 -0700

    rename albums.rs tabxb.rs

--- src/views.rs
+++ src/views.rs
@@ -1,15 +1,15 @@
-mod albums;
 pub mod context_menu;
 mod dropdown;
 mod kallax;
 pub mod tabxa;
 mod tabxa_tabxb;
+mod tabxb;
 mod tabxc_tabxd;
 
-pub use albums::Albums;
 pub use context_menu::ContextMenu;
 pub use dropdown::Dropdown;
 pub use kallax::Kallax;
 pub use tabxa::Tracks;
 pub use tabxa_tabxb::Browse;
+pub use tabxb::Albums;
 pub use tabxc_tabxd::NowPlaying;
