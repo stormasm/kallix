826965e9031709998f70edcdf8bc8f2da6152a31
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2024-04-05 08:27:17 -0700

    rename areas of the ui to part01 part02

--- src/views.rs
+++ src/views.rs
@@ -1,15 +1,15 @@
 mod dropdown;
 mod kallax;
 pub mod on_right_click;
+mod part01;
+mod part02;
 pub mod tabxa;
-mod tabxa_tabxb;
 mod tabxb;
-mod tabxc_tabxd;
 
 pub use dropdown::Dropdown;
 pub use kallax::Kallax;
 pub use on_right_click::ContextMenu;
+pub use part01::Browse;
+pub use part02::NowPlaying;
 pub use tabxa::Tracks;
-pub use tabxa_tabxb::Browse;
 pub use tabxb::Albums;
-pub use tabxc_tabxd::NowPlaying;
